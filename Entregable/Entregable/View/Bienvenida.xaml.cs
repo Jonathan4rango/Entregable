9766535ef699b4c4268b8d78161a88c77c0fbfb3
Jonathan Arango <jonathan.arango@utp.edu.co>
2023-04-24 21:49:43 -0500

    Agregar archivos de proyecto.

---
+++ Entregable/Entregable/View/Bienvenida.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Entregable
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Bienvenida : ContentPage
+    {
+
+        public Bienvenida()
+        {
+            InitializeComponent();
+
+        }
+
+        async void Btn_login_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+    }
+}
---
+++ Entregable/Entregable/View/Login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Entregable
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        async void Btn_Registro_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Registro());
+        }
+    }
+}
